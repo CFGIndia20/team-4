2e44bc004dd20ec8027923304667b4be694d24f4
Harshita <h2010singh@gmail.com>
2020-07-26 00:58:02 +0530

    Text Classifier wrapping customized

--- excalibur/botapi/textClassification.py
+++ excalibur/botapi/textClassification.py
@@ -5,11 +5,14 @@ import numpy as np
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+# from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.svm import LinearSVC
 # from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import SelectKBest, chi2
 
+# Reading the models
 data = pd.read_csv('./cd_mumbai_complaints.csv')
 print(data['category_id'].isnull().sum())
 y = data['category_id'].values.astype(np.int64)
@@ -17,6 +20,7 @@ z = data['sub_category_id']
 
 print(pd.DataFrame(y).info())
 
+# Preprocessing
 stemmer = SnowballStemmer('english')
 words = stopwords.words("english")
 
@@ -24,11 +28,11 @@ data['cleaned'] = data['description'].apply(lambda x: " ".join([stemmer.stem(i) 
 
 print(data['cleaned'].head(5))
 
+# Word Vectorization
 from sklearn.feature_extraction.text import CountVectorizer
 vectorizer = CountVectorizer(max_features=1500, min_df=5, max_df=0.7, stop_words=stopwords.words('english'))
 X = vectorizer.fit_transform(data['cleaned']).toarray()
 
-from sklearn.feature_extraction.text import TfidfTransformer
 tfidfconverter = TfidfTransformer()
 X = tfidfconverter.fit_transform(X).toarray()
 
@@ -51,21 +55,26 @@ print(classification_report(y_test,y_pred))
 print(accuracy_score(y_test, y_pred))
 
 def preprocessData(descp):
+    """
+        Preprocesses the sentences
+    """
     # Stopwords removed
-    descp = descp.apply(lambda x: " ".join([stemmer.stem(i) for i in re.sub("[^a-zA-Z]", " ", str(x)).split() if i not in words]).lower())
+    # descp = descp.apply(lambda x: " ".join([stemmer.stem(i) for i in re.sub("[^a-zA-Z]", " ", str(x)).split() if i not in words]).lower())
+    descp = [w.lower() for w in descp.split() if w not in words]
     
     # Words to bag
-    vectorizer = CountVectorizer(max_features=1500, min_df=5, max_df=0.7, stop_words=stopwords.words('english'))
+    vectorizer = CountVectorizer(max_features=1500, min_df=1, max_df=0.7, stop_words=stopwords.words('english'))
+    # vectorizer = TfidfVectorizer(min_df=1, max_df=1, stop_words=stopwords.words('english'))
     X = vectorizer.fit_transform(descp).toarray()
     
-    # Tfid Convertor
+    # Tfidf Convertor
     tfidfconverter = TfidfTransformer()
     X = tfidfconverter.fit_transform(X).toarray()
     
     return X
 
 def predictClass(x_pred):
-    return classifier.predict(X_test)
+    return classifier.predict(x_pred)
 
 # 
 # function textClassifier
@@ -75,5 +84,5 @@ def textClassifier(descp):
     """
         Predicts category from description
     """
-    processedDescp = preprocessData(descp=str(descp))
+    processedDescp = preprocessData(descp=descp)
     return int(predictClass(processedDescp))
