d2ddb176b83e175e3762118824f6f35146b599df
Harshita <h2010singh@gmail.com>
2020-07-25 16:34:30 +0530

    Whatsapp bot reply complete

--- excalibur/botapi/urls.py
+++ excalibur/botapi/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('whatsapp/', views.WhatsappBot.as_view(), name="whatsappbot")
+    # path('whatsapp/', views.captureUserMessage, name="whatsappbot")
 ]
--- excalibur/botapi/views.py
+++ excalibur/botapi/views.py
@@ -3,9 +3,11 @@ from django.http import Http404, HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.views.decorators.csrf import csrf_exempt
 
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
+from .env import ACCOUNT_SID, AUTH_TOKEN 
 
 # Create your views here.
 def index(request):
@@ -28,4 +30,13 @@ class WhatsappBot(APIView):
         response = MessagingResponse()
         msg = response.message("Hello world!")
         
-        return HttpResponse("Hello world") 
+        client = Client(ACCOUNT_SID, AUTH_TOKEN)
+
+        message = client.messages.create(
+            body='Hello there!',
+            from_='whatsapp:+14155238886',
+            to='whatsapp:+919920583257'
+        )
+
+        print(message)
+        return HttpResponse(str(msg)) 
