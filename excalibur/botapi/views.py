6f7f736c3c2767cbe690f0f173bdb1df1c759f32
Harshita <h2010singh@gmail.com>
2020-07-25 14:19:22 +0530

    Django boilerplate setup

--- excalibur/botapi/views.py
+++ excalibur/botapi/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+def index(request):
+    return Response({'index' : 'works'}, status=status.HTTP_200_OK)
